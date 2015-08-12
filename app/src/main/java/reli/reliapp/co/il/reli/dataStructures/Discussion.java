f7b53d49e8d429da0271d37bc3c3030c72de6e6e
Shachar Langer <shachar.langer@gmail.com>
2015-08-12 21:57:30 +0300

    Basic location features added

--- app/src/main/java/reli/reliapp/co/il/reli/dataStructures/Discussion.java
+++ app/src/main/java/reli/reliapp/co/il/reli/dataStructures/Discussion.java
@@ -4,9 +4,13 @@ import android.graphics.Bitmap;
 
 import com.parse.ParseClassName;
 import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.Date;
+
+import reli.reliapp.co.il.reli.utils.Const;
 
 @ParseClassName("Discussions")
 public class Discussion extends AbstractDiscussion {
@@ -21,6 +25,80 @@ public class Discussion extends AbstractDiscussion {
                       Bitmap discussionLogo, Date creationDate, Date expirationDate,
                       String ownerParseID) {
         super(discussionName, location, radius, discussionLogo, creationDate, expirationDate, ownerParseID);
+    }
+
+    public void setDiscussionName(String discussionName){
+        put(Const.COL_DISCUSSION_NAME, discussionName);
+    }
+
+    public String getDiscussionName() {
+        return getString(Const.COL_DISCUSSION_NAME);
+    }
+
+    public void setLocation(ParseGeoPoint location) {
+        put(Const.COL_DISCUSSION_LOCATION, location);
+    }
+
+    public ParseGeoPoint getLocation() {
+        return getParseGeoPoint(Const.COL_DISCUSSION_LOCATION);
+    }
+
+    public void setRadius(int radius) {
+        put(Const.COL_DISCUSSION_RADIUS, radius);
+    }
+
+    public int getRadius() {
+        return getInt(Const.COL_DISCUSSION_RADIUS);
+    }
+
+    public void setDiscussionLogo(Bitmap discussionLogo) {
+        // TODO
+    }
+
+    public Bitmap getDiscussionLogo() {
+        // TODO
+        return null;
+    }
+
+    // No setter for the creation date
+    public Date getCreationDate() {
+        return getDate(Const.COL_DISCUSSION_CREATION_DATE);
+    }
+
+    /* ========================================================================== */
+
+    public void setExpirationDate(Date expirationDate) {
+        put(Const.COL_DISCUSSION_EXPIRATION_DATE, expirationDate);
+    }
+
+    /* ========================================================================== */
+
+    public Date getExpirationDate() {
+        return getDate(Const.COL_DISCUSSION_EXPIRATION_DATE);
+    }
+
+    /* ========================================================================== */
+
+    public void setOwnerParseID(String ownerParseID) {
+        put(Const.COL_DISCUSSION_OWNER_PARSE_ID, ownerParseID);
+    }
+
+    /* ========================================================================== */
+
+    public String getOwnerParseID() {
+        return getString(Const.COL_DISCUSSION_OWNER_PARSE_ID);
+    }
+
+    /* ========================================================================== */
+
+    public String getParseID() {
+        return getObjectId();
+    }
+
+    /* ========================================================================== */
+
+    public static ParseQuery<Discussion> getDiscussionQuery() {
+        return ParseQuery.getQuery(Discussion.class);
     }
 }
 
